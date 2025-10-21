7d2eda33f3d3fe3df4dcb1ae915925dc464e1219
KiruhaRGD <kirilllukinskih@gmail.com>
2025-10-21 11:38:25 +0500

    'init'

--- backend/models/film.js
+++ backend/models/film.js
@@ -8,7 +8,22 @@ module.exports = {
     getbyid: async (id) => {
         result = await pool.query('SELECT * FROM film WHERE id = $1', [id])
     },
-    update: async (nazvanie,opis) => {
-        result = await pool.query('UPDATE film set ');
-    }
+     update: async (id, nazvanie, opisanie, duration, year_of_release, id_strana) => {
+    const result = await pool.query('UPDATE film SET nazvanie = $1, opisanie = $2, duration = $3, year_of_release = $4, id_strana = $5 WHERE id = $6', 
+        [nazvanie, opisanie, duration, year_of_release, id_strana, id]
+    );
+    return result;
+    },
+    insert: async (id, nazvanie, opisanie, duration, year_of_release, id_strana) => {
+    const result = await pool.query('INSERT film SET nazvanie = $1, opisanie = $2, duration = $3, year_of_release = $4, id_strana = $5 WHERE id = $6', 
+        [nazvanie, opisanie, duration, year_of_release, id_strana, id]
+    );
+    return result;
+    },
+    delete: async (id) => {
+    const result = await pool.query('DELETE FROM film WHERE id = $1', [id]);
+    return result;
+    },
 }
+
+    
